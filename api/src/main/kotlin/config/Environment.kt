0d3784b8748341fe31e9da0bad6ce965182025d9
lmint1 <lemint.dev@gmail.com>
2022-04-18 16:15:35 -0300

    Changes on Environment.kt and created .env.example

--- api/src/main/kotlin/config/Environment.kt
+++ api/src/main/kotlin/config/Environment.kt
@@ -1,11 +1,27 @@
 package config
 
+import java.lang.Exception
+import java.net.URI
+
 data class Environment(
-    val apikey: String = System.getenv("apikey"),
-    val baseUrl: String = System.getenv("baseUrl"),
-    val isDebug: Boolean = System.getenv("environment") == "DEBUG",
-    val dbHost: String = System.getenv("dbHost"),
-    val dbUserName: String = System.getenv("dbUserName"),
-    val dbPassword: String = System.getenv("dbPassword"),
-    val secret: String = System.getenv("secret"),
-)
+    val apikey: String = System.getenv("API_KEY"),
+    val baseUrl: String = System.getenv("BASE_URL"),
+    val isDebug: Boolean = System.getenv("IS_DEBUG") == "DEBUG",
+    var dbHost: String = System.getenv("DATABASE_URL"),
+    var dbUserName: String = getenvOrNull("DATABASE_USER") ?: "",
+    var dbPassword: String = getenvOrNull("DATABASE_PASSWORD") ?: "",
+    val secret: String = System.getenv("SECRET"),
+) {
+
+    init {
+        val dbUri = URI(dbHost)
+        dbUserName = dbUri.userInfo.split(":")[0]
+        dbPassword = dbUri.userInfo.split(":")[1]
+        dbHost = "jdbc:postgresql://" + dbUri.host + ':' + dbUri.port + dbUri.path.toString() + "?sslmode=require"
+    }
+
+}
+
+private fun getenvOrNull(name: String): String? {
+    return try { System.getenv(name) } catch (e: Exception) { null }
+}
